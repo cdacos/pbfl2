1a15a7c5171e164d595b7c3cadc6dc54d0903a2b
Carlos da Costa <git@cdacosta.com>
2023-05-29 12:23:05 +0100

    Renamed Pbfl.Blazor for front-end

--- Pbfl.Blazor/Program.cs
+++ Pbfl.Blazor/Program.cs
@@ -1,6 +1,6 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Pbfl.Web;
+using Pbfl.Blazor;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
