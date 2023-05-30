ee7f529b502ee49f743196a2169ad874d42d50f4
Carlos da Costa <git@cdacosta.com>
2023-05-30 13:04:14 +0100

    Example CRUD API

--- Pbfl.API/Program.cs
+++ Pbfl.API/Program.cs
@@ -1,6 +1,7 @@
-using Pbfl.API;
+using Microsoft.EntityFrameworkCore;
 using Pbfl.API.Helpers;
 using Pbfl.Data;
+using Pbfl.Data.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -21,7 +22,42 @@ app.UseHttpsRedirection();
 
 // https://learn.microsoft.com/en-us/aspnet/core/fundamentals/minimal-apis/openapi
 
+app.MapGet("/leagues", async (AppDbContext db) => await db.Leagues.ToListAsync());
+app.MapGet("/leagues/{id:int}", async (AppDbContext db, int id) => await db.Leagues.FindAsync(id));
+app.MapPost("/leagues", async (AppDbContext db, League league) =>
+{
+    await db.Leagues.AddAsync(league);
+    await db.SaveChangesAsync();
+    return Results.Created($"/leagues/{league.LeagueId}", league);
+});
+app.MapPut("/leagues/{id:int}", async (AppDbContext db, League updatedLeague, int id) =>
+{
+    var league = await db.Leagues.FindAsync(id);
+    if (league is null) return Results.NotFound();
+    league.Name = updatedLeague.Name;
+    league.Description = updatedLeague.Description;
+    await db.SaveChangesAsync();
+    return Results.NoContent();
+});
+app.MapDelete("/leagues/{id:int}", async (AppDbContext db, int id) =>
+{
+    var league = await db.Leagues.FindAsync(id);
+    if (league is null)
+    {
+        return Results.NotFound();
+    }
+    db.Leagues.Remove(league);
+    await db.SaveChangesAsync();
+    return Results.Ok();
+});
+
 app.MapGet("/", () => new string[] { "Hello", "World!" });
+
+app.MapGet("/health", async (AppDbContext db) =>
+{
+    var leagueCount = await db.Leagues.CountAsync();
+    return leagueCount > 0 ? Results.Ok(leagueCount) : Results.StatusCode(StatusCodes.Status503ServiceUnavailable);
+});
 
 app.MapGet("/swag", () => "Hello Swagger!")
     .WithOpenApi();
